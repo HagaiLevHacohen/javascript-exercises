92d857f18ae6571a7366f402f0b01bab1558c961
HagaiLevHacohen <levhacohen@mail.tau.ac.il>
2025-09-01 20:31:26 +0300

    arrays and loops

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0){
+        return "ERROR";
+    }
+    output_str = "";
+    for (let i = 0; i < num; i++){
+        output_str = output_str + str;
+    }
+    return output_str;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(startIdx, endIdx) {
+    if (!Number.isInteger(startIdx) || !Number.isInteger(endIdx)) return "ERROR";
+    if (startIdx < 0 || endIdx < 0) return "ERROR";
+    if (startIdx > endIdx) {
+        const temp = startIdx;
+        startIdx = endIdx;
+        endIdx = temp;
+    }
+    let sum = 0;
+    for(let i = startIdx; i<endIdx + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
